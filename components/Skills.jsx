c404bad46795169c43c83988c75eacc38cfb234d
venu95020 <venukoyyana908@gmail.com>
2025-12-02 23:19:34 +0530

    changes

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -22,7 +22,7 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src="/assets/skills/Javascript.png" width='64px' height='64px' alt='/' />
+                <Image src="/assets/skills/javascript.png" width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>JavaScript</h3>
